717f3e13d6299f59b22c6920e3633475aa1d1d84
Unzor <71789103+Unzor@users.noreply.github.com>
2022-05-20 22:52:16 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,39 +6,38 @@ app.use(express.json()), app.use(require("express-all-allow")());
 var fetch = require("node-fetch");
 
 function method(e) {
-    var a = "";
-    a = "get" == e ? "" : "req.body", app[e]("/" + e, async function(t, r) {
-        var s = generator.generate();
-        if ("" !== a) fetch(atob(t.query.url), {
+    var t;
+    t = "get" == e ? "" : "req.body", app[e]("/" + e, async function(a, r) {
+        var o = generator.generate();
+        "" !== t ? fetch(atob(a.query.url), {
             method: e.toUpperCase(),
-            body: a,
+            body: t,
             headers: {
-                "x-forwarded-for": s,
-                "x-client-ip": s
+                "x-forwarded-for": o,
+                "x-client-ip": o
             }
         }).then(e => e.text()).then(async e => {
-            var a = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + process.env.API_KEY + "&ip=" + s);
-            a = await a.json(), r.send({
+            var t = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + process.env.API_KEY + "&ip=" + o);
+            t = await t.json(), r.send({
                 data: e,
-                ip_data: a
+                ip_data: t
             })
         }).catch(e => {
             r.status(400).send({
                 error: !0,
                 message: e.message
             })
-        });
-        else fetch(atob(t.query.url), {
+        }) : fetch(atob(a.query.url), {
             method: e.toUpperCase(),
             headers: {
-                "x-forwarded-for": s,
-                "x-client-ip": s
+                "x-forwarded-for": o,
+                "x-client-ip": o
             }
         }).then(e => e.text()).then(async e => {
-            var a = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + process.env.API_KEY + "&ip=" + s);
-            a = await a.json(), r.send({
+            var t = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + process.env.API_KEY + "&ip=" + o);
+            t = await t.json(), r.send({
                 data: e,
-                ip_data: a
+                ip_data: t
             })
         }).catch(e => {
             r.status(400).send({
@@ -48,7 +47,49 @@ function method(e) {
         })
     })
 }
-method("get"), method("post"), method("put"), method("delete");
+
+function ipMethod(e) {
+    app[e]("/ip/" + e, async function(t, a) {
+        var r = e;
+        "get" == r.toLowerCase() || "head" == r.toLowerCase() ? fetch(atob(t.query.url), {
+            method: r.toUpperCase(),
+            headers: {
+                "x-forwarded-for": t.query.ip,
+                "x-client-ip": t.query.ip
+            }
+        }).then(e => e.text()).then(async e => {
+            var r = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + process.env.API_KEY + "&ip=" + t.query.ip);
+            r = await r.json(), a.send({
+                data: e,
+                ip_data: r
+            })
+        }).catch(e => {
+            a.status(400).send({
+                error: !0,
+                message: e.message
+            })
+        }) : fetch(atob(t.query.url), {
+            method: r.toUpperCase(),
+            headers: {
+                "x-forwarded-for": t.query.ip,
+                "x-client-ip": t.query.ip
+            },
+            body: t.body
+        }).then(e => e.text()).then(async e => {
+            var r = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + process.env.API_KEY + "&ip=" + t.query.ip);
+            r = await r.json(), a.send({
+                data: e,
+                ip_data: r
+            })
+        }).catch(e => {
+            a.status(400).send({
+                error: !0,
+                message: e.message
+            })
+        })
+    })
+}
+method("get"), method("post"), method("put"), method("delete"), ipMethod("get"), ipMethod("post"), ipMethod("put"), ipMethod("delete");
 const PORT = process.env.PORT || 3e3;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${PORT}`)
