7bd9ec6f6ec2b0fde782b68fb1e3e9ff94c2c644
Unzor <71789103+Unzor@users.noreply.github.com>
2022-05-20 22:51:34 -0500

    Update privia.js

--- client/privia.js
+++ client/privia.js
@@ -1,11 +1,12 @@
 class Privia {
     constructor(method, url) {
-    var thisp = this;
-    return new Promise(async () => {
-        var http = await fetch("https://priviaJS-server.seven7four4.repl.co/" + method + "?url=" + btoa(url));
-        http = await http.json();
-        thisp.data = http;
-    });
+        var thisp = this;
+        return new Promise(async (resolve) => {
+            var http = await fetch("https://priviaJS-server.seven7four4.repl.co/" + method + "?url=" + btoa(url));
+            http = await http.json();
+            thisp.data = http;
+            resolve(thisp);
+        });
     }
     getText() {
         return this.data.data
@@ -15,5 +16,17 @@ class Privia {
     }
     getIPData() {
         return this.data.ip_data
+    }
+    newRequest(method, url) {
+        var thisp = this;
+        return new Promise(async (resolve) => {
+            var http = await fetch("https://priviaJS-server.seven7four4.repl.co/ip/" + method + "?url=" + btoa(url) + "&ip=" + thisp.data.ip_data.ip);
+            http = await http.json();
+            thisp.data = http;
+            resolve(thisp);
+        });
+    }
+    async regenerate(method, url) {
+        return await new this.constructor(method, url);
     }
 }
